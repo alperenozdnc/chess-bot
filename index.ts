a9d3c4ddb4e071c6657f96719ea4ea22b8ef2b45
alperenozdnc <alperenozdincc@gmail.com>
2025-09-05 00:34:48 +0300

    chore: migrating to typescript

--- index.ts
+++ index.ts
@@ -1,273 +1,320 @@
-"use strict";
+type PieceColor = "white" | "black";
+
+interface MoveData {
+    ID: string;
+    color: PieceColor;
+    pieceMoveCount: number;
+    startSquare: HTMLDivElement;
+    destinationSquare: HTMLDivElement;
+}
+
 const FILES = "abcdefgh".split("");
 const INITIAL_POSITION = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
 const captureSound = new Audio("https://images.chesscomfiles.com/chess-themes/sounds/_MP3_/default/capture.mp3");
 const moveSound = new Audio("https://images.chesscomfiles.com/chess-themes/sounds/_MP3_/default/move-self.mp3");
+
 let isBoardFlipped = false;
-const flipButton = document.getElementById("flip-button");
-flipButton.addEventListener("click", () => {
-    isBoardFlipped = !isBoardFlipped;
+
+const flipButton = document.getElementById("flip-button") as HTMLButtonElement;
+
+flipButton!.addEventListener("click", () => {
+    isBoardFlipped = !isBoardFlipped
+
     drawBoardfromFEN(isBoardFlipped ? INITIAL_POSITION.split("").reverse().join("") : INITIAL_POSITION, isBoardFlipped);
 });
-function resetDraggedPieceStyles(draggedPiece) {
+
+function resetDraggedPieceStyles(draggedPiece: HTMLImageElement) {
     draggedPiece.classList.remove("dragged");
     draggedPiece.style.left = "";
     draggedPiece.style.top = "";
 }
-function drawBoardfromFEN(FEN, isBoardFlipped = false) {
-    const BOARD = document.getElementById("board-wrapper");
+
+function drawBoardfromFEN(FEN: string, isBoardFlipped = false) {
+    const BOARD = document.getElementById("board-wrapper") as HTMLDivElement;
+
     BOARD.innerHTML = "";
+
     let squareColor = 0;
     let FENIdx = 0;
+
     for (let row = 0; row < 8; ++row) {
         const ROW_ELEMENT = document.createElement(`div`);
         ROW_ELEMENT.id = `row - ${row}`;
         ROW_ELEMENT.className = "row";
+
         BOARD.appendChild(ROW_ELEMENT);
+
         let spacesLeft = 0;
+
         for (let square = 0; square < 8; ++square) {
             const SQUARE_ELEMENT = document.createElement(`div`);
             SQUARE_ELEMENT.id = `square - ${square}`;
-            SQUARE_ELEMENT.className = `square ${squareColor % 2 == 0 ? "white" : "black"}`;
+            SQUARE_ELEMENT.className = `square ${squareColor % 2 == 0 ? "white" : "black"}`
+
             if (isBoardFlipped) {
-                SQUARE_ELEMENT.dataset.pos = `${FILES[8 - square - 1]}${row + 1}`;
+                SQUARE_ELEMENT.dataset.pos = `${FILES[8 - square - 1]}${row + 1}`
+            } else {
+                SQUARE_ELEMENT.dataset.pos = `${FILES[square]}${8 - row}`
             }
-            else {
-                SQUARE_ELEMENT.dataset.pos = `${FILES[square]}${8 - row}`;
-            }
-            if (square !== 7)
-                ++squareColor;
+
+            if (square !== 7) ++squareColor;
+
             ROW_ELEMENT.appendChild(SQUARE_ELEMENT);
+
             if (FENIdx < FEN.length) {
-                if (FEN[FENIdx] == "/")
-                    FENIdx++;
+                if (FEN[FENIdx] == "/") FENIdx++;
+
                 if (spacesLeft > 0) {
                     spacesLeft--;
-                }
-                else {
+                } else {
                     let piece = FEN[FENIdx];
-                    if (isNaN(piece)) {
+
+                    if (isNaN(piece as unknown as number)) {
                         const IMAGE_ELEMENT = document.createElement("img");
                         IMAGE_ELEMENT.src = `./assets/${piece}.png`;
+
                         IMAGE_ELEMENT.dataset.color = piece.toLowerCase() === piece ? "black" : "white";
                         IMAGE_ELEMENT.dataset.pieceid = piece.toLowerCase();
-                        IMAGE_ELEMENT.dataset.move_count = 0;
+                        (IMAGE_ELEMENT.dataset.move_count as unknown as number) = 0;
+
                         IMAGE_ELEMENT.className = "piece";
                         IMAGE_ELEMENT.draggable = false;
+
                         SQUARE_ELEMENT.appendChild(IMAGE_ELEMENT);
-                    }
-                    else {
+                    } else {
                         spacesLeft += +piece - 1;
                     }
+
                     ++FENIdx;
                 }
             }
         }
     }
 }
+
 drawBoardfromFEN(INITIAL_POSITION);
-async function getPromotionSelection(color) {
-    const PROMOTION_SCREEN = document.getElementById("promotion-screen");
+
+async function getPromotionSelection(color: PieceColor) {
+    const PROMOTION_SCREEN = document.getElementById("promotion-screen") as HTMLDivElement;
     const PROMOTION_PIECES = ["q", "r", "n", "b"];
     const PROMOTION_PIECES_ELEMENTS = PROMOTION_SCREEN.querySelectorAll(".piece-selection");
+
     PROMOTION_SCREEN.classList.add("promotion-screen-visible");
+
     PROMOTION_PIECES.forEach((piece, idx) => {
         let pieceID = color === "black" ? piece : piece.toUpperCase();
+
         const IMAGE_ELEMENT = document.createElement("img");
         IMAGE_ELEMENT.src = `./assets/${pieceID}.png`;
         IMAGE_ELEMENT.draggable = false;
         IMAGE_ELEMENT.classList.add("promotion-piece");
+
         PROMOTION_PIECES_ELEMENTS[idx].appendChild(IMAGE_ELEMENT);
     });
-    const selectionPromise = new Promise((resolve) => {
-        PROMOTION_PIECES_ELEMENTS.forEach((button) => {
+
+    const selectionPromise = new Promise<string>((resolve) => {
+        PROMOTION_PIECES_ELEMENTS.forEach((button: Element) => {
             button.addEventListener("click", () => {
-                resolve((button.dataset.id));
+                resolve(((button as HTMLButtonElement).dataset.id) as string);
+
                 PROMOTION_SCREEN.classList.remove("promotion-screen-visible");
             });
         });
     });
+
     return selectionPromise.then(val => val);
 }
-async function checkLegality(data) {
+
+async function checkLegality(data: MoveData) {
     const { ID, color, pieceMoveCount, startSquare, destinationSquare } = data;
+
     let isMoveLegal = true;
-    const posA = startSquare.dataset.pos;
-    const posB = destinationSquare.dataset.pos;
+
+    const posA = startSquare.dataset.pos!;
+    const posB = destinationSquare.dataset.pos!;
+
     const fileA = FILES.indexOf(posA[0]);
     const fileB = FILES.indexOf(posB[0]);
+
     const rankA = +posA[1];
     const rankB = +posB[1];
+
     let isCapturing = false;
     let isPromoting = false;
+
     if (destinationSquare.innerHTML !== "") {
-        const capturedPiece = destinationSquare.children[0];
+        const capturedPiece = (destinationSquare.children[0] as HTMLImageElement);
+
         if (color === capturedPiece.dataset.color) {
             console.error("you cant capture your own piece");
             isMoveLegal = false;
         }
+
         if (capturedPiece.dataset.pieceid === "k") {
             console.error("you cant capture a king");
             isMoveLegal = false;
         }
+
         isCapturing = true;
     }
+
     switch (ID) {
         case "p":
             // todo: en passant
-            const dd = Math.abs(rankA - rankB);
-            if (!isCapturing && fileA !== fileB) {
-                console.error("cant change files when not capturing");
-                isMoveLegal = false;
-            }
-            ;
-            if (isCapturing && fileA === fileB) {
-                console.error("cant stay on the same file while capturing");
-                isMoveLegal = false;
-            }
-            ;
-            if (isCapturing && rankA === rankB) {
-                console.error("cant stay on the same rank while capturing");
-                isMoveLegal = false;
-            }
-            ;
-            if (isCapturing && Math.abs(fileA - fileB) > 1) {
-                console.error("can only capture to the left or right");
-                isMoveLegal = false;
-            }
-            if (color === "white" && rankA > rankB) {
-                console.error("cant decrease rank as white");
-                isMoveLegal = false;
-            }
-            ;
-            if (color === "black" && rankA < rankB) {
-                console.error("cant increase rank as black");
-                isMoveLegal = false;
-            }
-            ;
-            if (dd > 2) {
-                console.error("cant move for more than 2 squares");
-                isMoveLegal = false;
-            }
-            if (pieceMoveCount > 0 && dd > 1) {
-                console.error("cant move more than 1 square after first move");
-                isMoveLegal = false;
-            }
+
+            const dd: number = Math.abs(rankA - rankB);
+
+            if (!isCapturing && fileA !== fileB) { console.error("cant change files when not capturing"); isMoveLegal = false };
+            if (isCapturing && fileA === fileB) { console.error("cant stay on the same file while capturing"); isMoveLegal = false };
+            if (isCapturing && rankA === rankB) { console.error("cant stay on the same rank while capturing"); isMoveLegal = false };
+            if (isCapturing && Math.abs(fileA - fileB) > 1) { console.error("can only capture to the left or right"); isMoveLegal = false; }
+            if (color === "white" && rankA > rankB) { console.error("cant decrease rank as white"); isMoveLegal = false };
+            if (color === "black" && rankA < rankB) { console.error("cant increase rank as black"); isMoveLegal = false };
+            if (dd > 2) { console.error("cant move for more than 2 squares"); isMoveLegal = false; }
+            if (pieceMoveCount > 0 && dd > 1) { console.error("cant move more than 1 square after first move"); isMoveLegal = false; }
+
             if (color === "white" && rankB === 8) {
                 isPromoting = true;
-            }
-            else if (color === "black" && rankB === 1) {
+
+            } else if (color === "black" && rankB === 1) {
                 isPromoting = true;
             }
+
             if (isPromoting) {
                 // because the piece somehow keeps dragging when the modal pops up
-                resetDraggedPieceStyles(document.querySelector(".dragged"));
-                const selection = await getPromotionSelection(color);
+                resetDraggedPieceStyles(document.querySelector(".dragged")!);
+
+                const selection = await getPromotionSelection(color) as string;
+
                 const IMAGE_ELEMENT = document.createElement("img");
                 IMAGE_ELEMENT.src = `./assets/${color === "white" ? selection.toUpperCase() : selection}.png`;
+
                 IMAGE_ELEMENT.dataset.color = color;
                 IMAGE_ELEMENT.dataset.pieceid = selection;
-                IMAGE_ELEMENT.dataset.move_count = 0;
+                (IMAGE_ELEMENT.dataset.move_count as unknown as number) = 0;
+
                 IMAGE_ELEMENT.className = "piece";
                 IMAGE_ELEMENT.draggable = false;
+
                 destinationSquare.replaceChildren(IMAGE_ELEMENT);
                 startSquare.innerHTML = "";
             }
-            break;
+
+            break
         case "n":
-            break;
+            break
         case "b":
-            break;
+            break
         case "r":
-            break;
+            break
         case "q":
-            break;
+            break
         case "k":
-            break;
+            break
     }
+
     return { isMoveLegal, isCapturing, isPromoting };
 }
-let draggedPiece;
-let originalSquare;
+
+let draggedPiece: HTMLImageElement | null;
+let originalSquare: HTMLDivElement;
+
 let offsetX = 0;
 let offsetY = 0;
+
 let moveIdx = 0;
+
 document.addEventListener("mousedown", (e) => {
-    if (e.target.classList.contains("piece")) {
-        draggedPiece = e.target;
-        originalSquare = draggedPiece.parentElement;
+    if ((e.target as HTMLElement).classList.contains("piece")) {
+        draggedPiece = e.target as HTMLImageElement;
+        originalSquare = draggedPiece.parentElement as HTMLDivElement;
+
         const rect = draggedPiece.getBoundingClientRect();
+
         offsetX = e.clientX - rect.left;
         offsetY = e.clientY - rect.top;
+
         draggedPiece.classList.add("dragged");
+
         document.body.appendChild(draggedPiece);
         moveAt(e.pageX, e.pageY);
     }
-    function moveAt(pageX, pageY) {
+
+    function moveAt(pageX: number, pageY: number) {
         if (draggedPiece) {
             draggedPiece.style.left = `${pageX - offsetX}px`;
             draggedPiece.style.top = `${pageY - offsetY}px`;
         }
     }
+
     document.addEventListener("mousemove", onMouseMove);
-    function onMouseMove(e) {
+
+    function onMouseMove(e: MouseEvent) {
         moveAt(e.pageX, e.pageY);
     }
+
     document.addEventListener("mouseup", async function onMouseUp(e) {
-        var _a;
         if (draggedPiece) {
-            let target = document.elementFromPoint(e.clientX, e.clientY);
-            target = (target.classList.contains("piece") ? target.parentElement : target);
-            if (!target)
-                return;
+            let target = document.elementFromPoint(e.clientX, e.clientY)!;
+            target = (target.classList.contains("piece") ? target.parentElement : target)!;
+
+            if (!target) return;
             if (!target.classList.contains("square")) {
                 originalSquare.appendChild(draggedPiece);
                 resetDraggedPieceStyles(draggedPiece);
                 return;
             }
+
             let pieceCanMove = false;
-            const pieceColor = draggedPiece.dataset.color;
+            const pieceColor = draggedPiece.dataset.color as PieceColor;
+
             if (pieceColor === "white") {
-                if (moveIdx % 2 === 0)
-                    pieceCanMove = true;
+                if (moveIdx % 2 === 0) pieceCanMove = true;
+            } else {
+                if (moveIdx % 2 !== 0) pieceCanMove = true;
             }
-            else {
-                if (moveIdx % 2 !== 0)
-                    pieceCanMove = true;
-            }
-            let pieceid = draggedPiece.dataset.pieceid.toUpperCase();
+
+            let pieceid = draggedPiece.dataset.pieceid!.toUpperCase();
+
             const { isMoveLegal, isCapturing, isPromoting } = await checkLegality({
                 ID: pieceid.toLowerCase(),
                 color: pieceColor,
                 pieceMoveCount: Number(draggedPiece.dataset.move_count),
                 startSquare: originalSquare,
-                destinationSquare: target
+                destinationSquare: target as HTMLDivElement
             });
+
             if (target.classList.contains("square") && pieceCanMove && target !== originalSquare && isMoveLegal) {
-                let pos = target.dataset.pos;
+                let pos = (target as HTMLDivElement).dataset.pos;
+
                 let notation = pos;
-                if (pieceid !== "P")
-                    notation = `${pieceid}${notation}`;
-                draggedPiece.dataset.move_count = +((_a = draggedPiece.dataset.move_count) !== null && _a !== void 0 ? _a : 0) + 1;
+                if (pieceid !== "P") notation = `${pieceid}${notation}`;
+
+                (draggedPiece.dataset.move_count as unknown as number) = +(draggedPiece.dataset.move_count ?? 0) + 1;
+
                 ++moveIdx;
+
                 if (!isPromoting) {
                     target.innerHTML = "";
                     target.appendChild(draggedPiece);
                 }
+
                 if (isCapturing) {
                     captureSound.play();
-                }
-                else {
+                } else {
                     moveSound.play();
                 }
-            }
-            else {
+            } else {
                 originalSquare.appendChild(draggedPiece);
             }
+
             resetDraggedPieceStyles(draggedPiece);
         }
+
         document.removeEventListener("mousemove", onMouseMove);
         document.removeEventListener("mouseup", onMouseUp);
+
         draggedPiece = null;
     });
 });
+

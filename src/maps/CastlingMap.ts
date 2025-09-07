026a0e19f1803eb416ea9826f11cdcc6ba08265e
alperenozdnc <alperenozdincc@gmail.com>
2025-09-07 18:22:45 +0300

    chore: map for castling squares

---
+++ src/maps/CastlingMap.ts
@@ -0,0 +1,7 @@
+export const CastlingMap = new Map<string, string[]>([
+    ["c1", ["d1", "c1", "b1", "a1"]],
+    ["g1", ["f1", "g1", "h1"]],
+    ["c8", ["d8", "c8", "b8", "a8"]],
+    ["g8", ["f8", "g8", "h8"]],
+]);
+
---
+++ src/maps/index.ts
@@ -0,0 +1,1 @@
+export { CastlingMap } from "./CastlingMap";

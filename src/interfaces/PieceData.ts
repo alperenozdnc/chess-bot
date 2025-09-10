85e01c8cad41144c43d8411aecad26fb17c1d865
alperenozdnc <alperenozdincc@gmail.com>
2025-09-10 19:01:02 +0300

    chore: piece data interface

---
+++ src/interfaces/PieceData.ts
@@ -0,0 +1,4 @@
+export interface PieceData {
+    pos: string;
+    id: string;
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,4 @@ export { MoveData } from "./MoveData";
 export { MoveLegality } from "./MoveLegality";
 export { LegalMoveData } from "./LegalMoveData";
 export { SquareAndPiece } from "./SquareAndPiece";
+export { PieceData } from "./PieceData";

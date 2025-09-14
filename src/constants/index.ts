8aa1f96dc0266e8f444afb17cb4f720c982b647a
alperenozdnc <alperenozdincc@gmail.com>
2025-09-14 12:03:08 +0300

    chore: add check sound to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,6 +6,9 @@ export const CAPTURE_SOUND = new Audio(
 export const MOVE_SOUND = new Audio(
     "https://images.chesscomfiles.com/chess-themes/sounds/_MP3_/default/move-self.mp3",
 );
+export const CHECK_SOUND = new Audio(
+    "https://images.chesscomfiles.com/chess-themes/sounds/_WEBM_/default/move-check.webm",
+);
 export const MATE_SOUND = new Audio(
     "https://images.chesscomfiles.com/chess-themes/sounds/_WEBM_/default/game-end.webm",
 );

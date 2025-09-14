a78071276a9e8803220f62880ff9159e8126842b
alperenozdnc <alperenozdincc@gmail.com>
2025-09-14 18:17:36 +0300

    chore: remove `console.log` forgor

--- src/utils/resetDraggedPieceStyles.ts
+++ src/utils/resetDraggedPieceStyles.ts
@@ -1,6 +1,4 @@
 export function resetDraggedPieceStyles(draggedPiece: HTMLImageElement) {
-    console.log(draggedPiece);
-
     draggedPiece.classList.remove("dragged");
     draggedPiece.style.left = "";
     draggedPiece.style.top = "";

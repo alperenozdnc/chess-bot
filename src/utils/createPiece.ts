38c467a4bcc2eb65ef8b3aab80b86495d41441ac
alperenozdnc <alperenozdincc@gmail.com>
2025-09-18 22:45:31 +0300

    refactor: dynamic piece sizing

--- src/utils/createPiece.ts
+++ src/utils/createPiece.ts
@@ -5,9 +5,13 @@ export function createPiece(data: PieceData): void {
     const { pos, id } = data;
     const { square } = getSquareAndPieceFromPos(pos) as SquareAndPiece;
     const imageElement = document.createElement("img");
+    const w = square.offsetWidth;
+    const h = square.offsetHeight;
 
     imageElement.src = `./assets/${id}.png`;
 
+    imageElement.width = w;
+    imageElement.height = h;
     imageElement.dataset.color = id.toLowerCase() === id ? "black" : "white";
     imageElement.dataset.pieceid = id.toLowerCase();
     (imageElement.dataset.move_count as unknown as number) = 0;

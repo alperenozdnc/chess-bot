3c03d60bafacab941fb92ce5082bab071acc1bb9
alperenozdnc <alperenozdincc@gmail.com>
2025-09-18 19:46:37 +0300

    refactor: move move making logic into `makeMove` function POSSIBLY BREAKING

--- src/functions/handlePieceMovement.ts
+++ src/functions/handlePieceMovement.ts
@@ -136,6 +136,148 @@ function mutateDrawCounters(
     return { newMovesSincePawnAdvance, newMovesSinceCapture };
 }
 
+async function makeMove(
+    draggedPiece: HTMLImageElement | null,
+    moveIdx: number,
+    e: MouseEvent,
+    highlightedSquares: HTMLDivElement[],
+    originalSquare: HTMLDivElement,
+    movesSinceCapture: number,
+    movesSincePawnAdvance: number,
+    FENPositions: string[],
+) {
+    if (!draggedPiece) return;
+
+    const pieceColor = draggedPiece.dataset.color as PieceColor;
+    let pieceCanMove = checkTurn(moveIdx, pieceColor);
+    let target = document.elementFromPoint(e.clientX, e.clientY)!;
+    target = (
+        target.classList.contains("piece") ? target.parentElement : target
+    )!;
+
+    clearHighlights(highlightedSquares);
+
+    if (isTargetWrong(target, originalSquare, draggedPiece)) return;
+
+    if (!pieceCanMove) {
+        undoMove(originalSquare, draggedPiece);
+        resetDraggedPieceStyles(draggedPiece);
+        return;
+    }
+
+    let pieceid = draggedPiece.dataset.pieceid!.toUpperCase();
+
+    const {
+        isMoveLegal,
+        isCapturing,
+        isPromoting,
+        isCastling,
+        isEnPassant,
+        isChecking,
+        enPassantablePawn,
+    } = await checkLegality({
+        ID: pieceid.toLowerCase() as Piece,
+        color: pieceColor,
+        pieceElement: draggedPiece,
+        pieceMoveCount: Number(draggedPiece.dataset.move_count),
+        startSquare: originalSquare,
+        destinationSquare: target as HTMLDivElement,
+        moveIdx: moveIdx,
+        isJustChecking: false,
+    });
+
+    if (!isMoveLegal) {
+        undoMove(originalSquare, draggedPiece);
+        resetDraggedPieceStyles(draggedPiece);
+        return;
+    }
+
+    let pos = (target as HTMLDivElement).dataset.pos;
+
+    (draggedPiece.dataset.move_count as unknown as number) =
+        +(draggedPiece.dataset.move_count ?? 0) + 1;
+
+    ++moveIdx;
+
+    if (!isPromoting) {
+        target.innerHTML = "";
+        target.appendChild(draggedPiece);
+    } else {
+        resetDraggedPieceStyles(document.querySelector(".dragged")!);
+
+        const selection = (await getPromotionSelection(pieceColor)) as string;
+
+        target.innerHTML = "";
+        originalSquare.innerHTML = "";
+
+        createPiece({
+            id: pieceColor === "white" ? selection.toUpperCase() : selection,
+            pos: (target as HTMLDivElement).dataset.pos!,
+        });
+    }
+
+    document
+        .querySelectorAll(".move-highlight")
+        .forEach((element) => element.classList.remove("move-highlight"));
+    originalSquare.classList.add("move-highlight");
+    target.classList.add("move-highlight");
+
+    handleAudio(isCapturing, isChecking);
+
+    const { newMovesSinceCapture, newMovesSincePawnAdvance } =
+        mutateDrawCounters(
+            movesSinceCapture,
+            movesSincePawnAdvance,
+            isCapturing,
+            pieceid,
+        );
+
+    movesSinceCapture = newMovesSinceCapture;
+    movesSincePawnAdvance = newMovesSincePawnAdvance;
+
+    if (isCastling) {
+        const castlingSquares = CastlingMap.get(pos!)!;
+        const posA = castlingSquares[castlingSquares.length - 1];
+        const posB = castlingSquares[0];
+
+        const { square: squareFirst, piece: pieceFirst } =
+            getSquareAndPieceFromPos(posA) as SquareAndPiece;
+        const { square: squareSecond } = getSquareAndPieceFromPos(
+            posB,
+        ) as SquareAndPiece;
+        const rook = pieceFirst!;
+
+        (rook.dataset.move_count as unknown as number) =
+            +(rook.dataset.move_count ?? 0) + 1;
+
+        squareFirst.innerHTML = "";
+        squareSecond.appendChild(rook);
+    }
+
+    if (isEnPassant && enPassantablePawn) {
+        enPassantablePawn.remove();
+    }
+
+    FENPositions.push(getFEN());
+
+    const { isGameOver, FENPositions: positions } = await checkIfGameOver(
+        moveIdx,
+        pieceColor,
+        isChecking,
+        FENPositions,
+        movesSincePawnAdvance,
+        movesSinceCapture,
+    );
+
+    if (isGameOver) FENPositions = positions;
+
+    resetDraggedPieceStyles(draggedPiece);
+
+    draggedPiece = null;
+
+    return { moveIdx, movesSinceCapture, movesSincePawnAdvance, FENPositions };
+}
+
 export function handlePieceMovement() {
     let FENPositions: string[] = [INITIAL_POSITION];
 
@@ -190,145 +332,26 @@ export function handlePieceMovement() {
         }
 
         document.addEventListener("mouseup", async function onMouseUp(e) {
-            if (!draggedPiece) return;
+            const move = await makeMove(
+                draggedPiece,
+                moveIdx,
+                e,
+                highlightedSquares,
+                originalSquare,
+                movesSinceCapture,
+                movesSincePawnAdvance,
+                FENPositions,
+            );
 
-            const pieceColor = draggedPiece.dataset.color as PieceColor;
-            let pieceCanMove = checkTurn(moveIdx, pieceColor);
-            let target = document.elementFromPoint(e.clientX, e.clientY)!;
-            target = (
-                target.classList.contains("piece")
-                    ? target.parentElement
-                    : target
-            )!;
-
-            clearHighlights(highlightedSquares);
-
-            if (isTargetWrong(target, originalSquare, draggedPiece)) return;
-
-            if (!pieceCanMove) {
-                undoMove(originalSquare, draggedPiece);
-                resetDraggedPieceStyles(draggedPiece);
-                return;
+            if (move) {
+                moveIdx = move.moveIdx;
+                movesSinceCapture = move.movesSinceCapture;
+                movesSincePawnAdvance = move.movesSincePawnAdvance;
+                FENPositions = move.FENPositions;
             }
 
-            let pieceid = draggedPiece.dataset.pieceid!.toUpperCase();
-
-            const {
-                isMoveLegal,
-                isCapturing,
-                isPromoting,
-                isCastling,
-                isEnPassant,
-                isChecking,
-                enPassantablePawn,
-            } = await checkLegality({
-                ID: pieceid.toLowerCase() as Piece,
-                color: pieceColor,
-                pieceElement: draggedPiece,
-                pieceMoveCount: Number(draggedPiece.dataset.move_count),
-                startSquare: originalSquare,
-                destinationSquare: target as HTMLDivElement,
-                moveIdx: moveIdx,
-                isJustChecking: false,
-            });
-
-            if (!isMoveLegal) {
-                undoMove(originalSquare, draggedPiece);
-                resetDraggedPieceStyles(draggedPiece);
-                return;
-            }
-
-            let pos = (target as HTMLDivElement).dataset.pos;
-
-            (draggedPiece.dataset.move_count as unknown as number) =
-                +(draggedPiece.dataset.move_count ?? 0) + 1;
-
-            ++moveIdx;
-
-            if (!isPromoting) {
-                target.innerHTML = "";
-                target.appendChild(draggedPiece);
-            } else {
-                resetDraggedPieceStyles(document.querySelector(".dragged")!);
-
-                const selection = (await getPromotionSelection(
-                    pieceColor,
-                )) as string;
-
-                target.innerHTML = "";
-                originalSquare.innerHTML = "";
-
-                createPiece({
-                    id:
-                        pieceColor === "white"
-                            ? selection.toUpperCase()
-                            : selection,
-                    pos: (target as HTMLDivElement).dataset.pos!,
-                });
-            }
-
-            document
-                .querySelectorAll(".move-highlight")
-                .forEach((element) =>
-                    element.classList.remove("move-highlight"),
-                );
-            originalSquare.classList.add("move-highlight");
-            target.classList.add("move-highlight");
-
-            handleAudio(isCapturing, isChecking);
-
-            const { newMovesSinceCapture, newMovesSincePawnAdvance } =
-                mutateDrawCounters(
-                    movesSinceCapture,
-                    movesSincePawnAdvance,
-                    isCapturing,
-                    pieceid,
-                );
-
-            movesSinceCapture = newMovesSinceCapture;
-            movesSincePawnAdvance = newMovesSincePawnAdvance;
-
-            if (isCastling) {
-                const castlingSquares = CastlingMap.get(pos!)!;
-                const posA = castlingSquares[castlingSquares.length - 1];
-                const posB = castlingSquares[0];
-
-                const { square: squareFirst, piece: pieceFirst } =
-                    getSquareAndPieceFromPos(posA) as SquareAndPiece;
-                const { square: squareSecond } = getSquareAndPieceFromPos(
-                    posB,
-                ) as SquareAndPiece;
-                const rook = pieceFirst!;
-
-                (rook.dataset.move_count as unknown as number) =
-                    +(rook.dataset.move_count ?? 0) + 1;
-
-                squareFirst.innerHTML = "";
-                squareSecond.appendChild(rook);
-            }
-
-            if (isEnPassant && enPassantablePawn) {
-                enPassantablePawn.remove();
-            }
-
-            FENPositions.push(getFEN());
-
-            const { isGameOver, FENPositions: positions } =
-                await checkIfGameOver(
-                    moveIdx,
-                    pieceColor,
-                    isChecking,
-                    FENPositions,
-                    movesSincePawnAdvance,
-                    movesSinceCapture,
-                );
-
-            if (isGameOver) FENPositions = positions;
-
-            resetDraggedPieceStyles(draggedPiece);
             document.removeEventListener("mousemove", onMouseMove);
             document.removeEventListener("mouseup", onMouseUp);
-            draggedPiece = null;
         });
     });
 }

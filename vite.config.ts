ad67047d32a0b758114030bb5e828bfc23273265
alperenozdnc <alperenozdincc@gmail.com>
2025-09-24 20:02:50 +0300

    chore: add `testing` directory to paths

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
             "@functions": resolve(__dirname, "src/functions"),
             "@utils": resolve(__dirname, "src/utils"),
             "@maps": resolve(__dirname, "src/maps"),
+            "@testing": resolve(__dirname, "src/testing"),
         },
     },
     server: {

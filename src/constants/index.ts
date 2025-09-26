83f8d64721c7b93a34921fe9ba4e8dd32f37272b
alperenozdnc <alperenozdincc@gmail.com>
2025-09-26 21:52:31 +0300

    refactor: better readability

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,15 +1,23 @@
 export const FILES = "abcdefgh".split("");
 export const INITIAL_POSITION = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
-export const CAPTURE_SOUND = new Audio(
-    "https://images.chesscomfiles.com/chess-themes/sounds/_MP3_/default/capture.mp3",
-);
-export const MOVE_SOUND = new Audio(
-    "https://images.chesscomfiles.com/chess-themes/sounds/_MP3_/default/move-self.mp3",
-);
-export const CHECK_SOUND = new Audio(
-    "https://images.chesscomfiles.com/chess-themes/sounds/_WEBM_/default/move-check.webm",
-);
-export const MATE_SOUND = new Audio(
-    "https://images.chesscomfiles.com/chess-themes/sounds/_WEBM_/default/game-end.webm",
-);
 export const BOARD = document.getElementById("board-wrapper") as HTMLDivElement;
+
+export const CAPTURE_SOUND = new Audio(
+    "https://images.chesscomfiles.com/" +
+    "chess-themes/sounds/_MP3_/default/capture.mp3",
+);
+
+export const MOVE_SOUND = new Audio(
+    "https://images.chesscomfiles.com/" +
+    "chess-themes/sounds/_MP3_/default/move-self.mp3",
+);
+
+export const CHECK_SOUND = new Audio(
+    "https://images.chesscomfiles.com/" +
+    "chess-themes/sounds/_WEBM_/default/move-check.webm",
+);
+
+export const MATE_SOUND = new Audio(
+    "https://images.chesscomfiles.com/" +
+    "chess-themes/sounds/_WEBM_/default/game-end.webm",
+);

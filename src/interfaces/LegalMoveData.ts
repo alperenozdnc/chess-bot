5266c6d387518a17919247bcf4ce78dfcf1fde05
alperenozdnc <alperenozdincc@gmail.com>
2025-09-27 19:03:26 +0300

    chore: add more data

--- src/interfaces/LegalMoveData.ts
+++ src/interfaces/LegalMoveData.ts
@@ -1,6 +1,13 @@
+import { PieceData } from "./PieceData";
+
 export interface LegalMoveData {
     pos: string;
     isCapturing: boolean;
+    piece: PieceData;
+    isCastling: boolean;
+    isPromoting: boolean;
+    isEnPassant: boolean;
+    enPassantablePawnPos?: string;
 }
 
 export interface LegalMoveDataWithDOM extends LegalMoveData {

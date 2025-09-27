fa55aa97abc431b7fa7164df205d105f50a782de
alperenozdnc <alperenozdincc@gmail.com>
2025-09-27 13:29:31 +0300

    refactor: no DOM lookups

--- src/functions/checkForCheckmate.ts
+++ src/functions/checkForCheckmate.ts
@@ -1,31 +1,23 @@
 import { PieceColor } from "@types";
 import { listLegalMoves } from "./listLegalMoves";
 import { FILES } from "@constants";
-import { getSquareAndPieceFromPos } from "@utils";
-import { GameState, SquareAndPiece } from "@interfaces";
+import { GameState } from "@interfaces";
 
 export async function checkForCheckmate(state: GameState, color: PieceColor) {
     for (const file of FILES) {
         for (let rank = 1; rank <= 8; ++rank) {
-            const squareAndPieceData = getSquareAndPieceFromPos(
-                `${file}${rank}`,
-            ) as SquareAndPiece;
+            const piece = state.Board.find(
+                (piece) => piece.pos === file + rank,
+            );
 
-            if (!squareAndPieceData.square) continue;
-            if (!squareAndPieceData.piece) continue;
-            if (squareAndPieceData.piece.dataset.color !== color) continue;
+            if (!piece) continue;
+            if (piece.color !== color) continue;
 
-            state.draggedPiece = squareAndPieceData.piece;
-            state.originalSquare = squareAndPieceData.square;
+            const moves = await listLegalMoves(state, piece);
 
-            const legalMoves = await listLegalMoves({ state });
-
-            if (legalMoves.length > 0) return false;
+            if (moves.length > 0) return false;
         }
     }
-
-    state.draggedPiece = null;
-    state.originalSquare = undefined;
 
     return true;
 }

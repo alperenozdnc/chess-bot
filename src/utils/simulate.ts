1d49800d92f3962eef3f0e54cc87b14cfd4a972e
alperenozdnc <alperenozdincc@gmail.com>
2025-09-27 13:29:49 +0300

    fix: stale values after simulation decided to just clone the state instead of mutating and unmutating which was creating unintended side effects

--- src/utils/simulate.ts
+++ src/utils/simulate.ts
@@ -1,36 +1,132 @@
 import { GameState, PieceData } from "@interfaces";
+import { updateBoard } from "./updateBoard";
+import { initGameState } from "@functions";
+import { CastlingMap } from "@maps";
+import { Pieces } from "@enums";
 
 export function simulate(
     state: GameState,
     piece: PieceData,
     destinationPos: string,
+    isCastling: boolean,
+    isPromoting: boolean,
     isCapturing: boolean,
     isEnPassant: boolean,
+    enPassantablePawn?: PieceData | undefined,
 ) {
-    const originalPos = piece.pos;
-    let capturedPiece: PieceData | undefined;
+    const simulatedState = initGameState();
+    const simulatedBoard = structuredClone(state.Board);
+
+    simulatedState.Board = simulatedBoard;
+
+    const simulatedPiece = simulatedState.Board.find(
+        (p) => p.pos === piece.pos,
+    )!;
 
     function play() {
-        piece.pos = destinationPos;
+        simulatedPiece.moveCount += 1;
 
-        if (isCapturing && !isEnPassant) {
-            capturedPiece = state.Board.find((p) => p.pos === destinationPos)!;
+        let keepMoving = true;
 
-            state.Board.splice(state.Board.indexOf(capturedPiece), 1);
+        if (isPromoting) {
+            updateBoard(
+                {
+                    type: "PROMOTE",
+                    data: {
+                        piece: simulatedPiece,
+                        destinationPos,
+                        promoteTo: Pieces.Queen,
+                        isCapturing,
+                        capturedPiece: simulatedState.Board.find(
+                            (piece) => piece.pos === destinationPos,
+                        ),
+                    },
+                },
+                simulatedState,
+            );
+            keepMoving = false;
         }
 
-        piece.moveCount += 1;
-    }
+        if (keepMoving && isCastling && !isCapturing) {
+            const castlingSquares = CastlingMap.get(destinationPos!)!;
+            const posA = castlingSquares[castlingSquares.length - 1];
+            const posB = castlingSquares[0];
 
-    function unplay() {
-        piece.pos = originalPos;
+            updateBoard(
+                {
+                    type: "CASTLE",
+                    data: {
+                        king: {
+                            piece: simulatedPiece,
+                            destinationPos: destinationPos,
+                        },
+                        rook: {
+                            piece: simulatedState.Board.find(
+                                (piece) => piece.pos === posA,
+                            )!,
+                            destinationPos: posB,
+                        },
+                    },
+                },
+                simulatedState,
+            );
 
-        if (isCapturing && !isEnPassant && capturedPiece) {
-            state.Board.push(capturedPiece);
+            keepMoving = false;
         }
 
-        piece.moveCount -= 1;
+        if (keepMoving && !isCapturing) {
+            updateBoard(
+                {
+                    type: "MOVE",
+                    data: {
+                        piece: simulatedPiece,
+                        destinationPos: destinationPos,
+                    },
+                },
+                simulatedState,
+            );
+        } else {
+            let simulatedEnPassantablePawnPos;
+
+            if (enPassantablePawn) {
+                simulatedEnPassantablePawnPos = enPassantablePawn.pos;
+            }
+
+            updateBoard(
+                {
+                    type: "CAPTURE",
+                    data: {
+                        piece: simulatedPiece,
+                        destinationPos: destinationPos,
+                        capturedPiece: simulatedState.Board.find(
+                            (p) =>
+                                p.pos ===
+                                (!isEnPassant
+                                    ? destinationPos
+                                    : simulatedEnPassantablePawnPos!),
+                        )!,
+                        isEnPassant,
+                    },
+                },
+                simulatedState,
+            );
+        }
+
+        const king = simulatedState.Board.find(
+            (p) => p.id === "k" && p.color === piece.color,
+        );
+
+        const enemyKing = simulatedState.Board.find(
+            (p) => p.id === "k" && p.color !== piece.color,
+        );
+
+        if (!king || !enemyKing) return console.error("no king/enemy king");
+
+        simulatedState.kingPos = king!.pos;
+        simulatedState.enemyKingPos = enemyKing!.pos;
+
+        return simulatedState;
     }
 
-    return { play, unplay };
+    return { play };
 }

289ecc21497b68a03cdbf7aa17c36f5264f26b68
alperenozdnc <alperenozdincc@gmail.com>
2025-10-01 23:52:29 +0300

    feat: square value integration for minimax

--- src/functions/evaluate.ts
+++ src/functions/evaluate.ts
@@ -1,12 +1,47 @@
+import { FILES } from "@constants";
+import { Pieces } from "@enums";
 import { GameState } from "@interfaces";
+import { SquareValueMap } from "@maps";
 import { countMaterials } from "@utils";
 
 export function evaluate(state: GameState): number {
+    let evaluation = 0;
+    let isEndgame = false;
+
     const { white, black } = countMaterials(state);
 
     if (state.botColor === "white") {
-        return white - black;
+        evaluation += white - black;
     } else {
-        return black - white;
+        evaluation += black - white;
     }
+
+    if (white + black <= 18) isEndgame = true;
+
+    for (const [pos, piece] of state.Board) {
+        const fileChar = pos[0];
+        const rankIdx = Number(pos[1]);
+
+        let mapIdx = (8 - rankIdx) * 8 + FILES.indexOf(fileChar);
+
+        // square values are reversed for black
+        // 0-based total square count is 63
+        if (piece.color === "black") {
+            mapIdx = 63 - mapIdx;
+        }
+
+        let key: string = piece.id;
+
+        // the king should be more inclined to move towards the endgame
+        if (key === Pieces.King && isEndgame) {
+            (key as string) += "endgame";
+        }
+
+        const sign = piece.color === state.botColor ? 1 : -1;
+        const squareValue = sign * SquareValueMap.get(key)![mapIdx];
+
+        evaluation += squareValue;
+    }
+
+    return evaluation;
 }

8312e7a0931be3c59b1beeefcb48445f4f2110f0
alperenozdnc <alperenozdincc@gmail.com>
2025-10-02 00:39:35 +0300

    refactor: extract square value calculation

--- src/functions/evaluate.ts
+++ src/functions/evaluate.ts
@@ -1,8 +1,38 @@
 import { FILES } from "@constants";
 import { Pieces } from "@enums";
-import { GameState } from "@interfaces";
+import { GameState, PieceData } from "@interfaces";
 import { SquareValueMap } from "@maps";
 import { countMaterials } from "@utils";
+
+function getSquareValueForPiece(
+    state: GameState,
+    piece: PieceData,
+    isEndgame: boolean,
+) {
+    const pos = piece.pos;
+    const fileChar = pos[0];
+    const rankIdx = Number(pos[1]);
+
+    let mapIdx = (8 - rankIdx) * 8 + FILES.indexOf(fileChar);
+
+    // square values are reversed for black
+    // 0-based total square count is 63
+    if (piece.color === "black") {
+        mapIdx = 63 - mapIdx;
+    }
+
+    let key: string = piece.id;
+
+    // the king should be more inclined to move towards the endgame
+    if (key === Pieces.King && isEndgame) {
+        (key as string) += "endgame";
+    }
+
+    const sign = piece.color === state.botColor ? 1 : -1;
+    const value = sign * SquareValueMap.get(key)![mapIdx];
+
+    return value;
+}
 
 export function evaluate(state: GameState): number {
     let evaluation = 0;
@@ -18,29 +48,8 @@ export function evaluate(state: GameState): number {
 
     if (white + black <= 18) isEndgame = true;
 
-    for (const [pos, piece] of state.Board) {
-        const fileChar = pos[0];
-        const rankIdx = Number(pos[1]);
-
-        let mapIdx = (8 - rankIdx) * 8 + FILES.indexOf(fileChar);
-
-        // square values are reversed for black
-        // 0-based total square count is 63
-        if (piece.color === "black") {
-            mapIdx = 63 - mapIdx;
-        }
-
-        let key: string = piece.id;
-
-        // the king should be more inclined to move towards the endgame
-        if (key === Pieces.King && isEndgame) {
-            (key as string) += "endgame";
-        }
-
-        const sign = piece.color === state.botColor ? 1 : -1;
-        const squareValue = sign * SquareValueMap.get(key)![mapIdx];
-
-        evaluation += squareValue;
+    for (const [_, piece] of state.Board) {
+        evaluation += getSquareValueForPiece(state, piece, isEndgame);
     }
 
     return evaluation;

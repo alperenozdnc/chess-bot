c2a70edf7128fc80453baeca94c68af9166700d6
alperenozdnc <alperenozdincc@gmail.com>
2025-10-01 23:47:41 +0300

    refactor: add center biased ordering

--- src/functions/heuristicScore.ts
+++ src/functions/heuristicScore.ts
@@ -1,20 +1,40 @@
+import { FILES } from "@constants";
 import { Values } from "@enums";
 import { LegalMoveData } from "@interfaces";
 
 export function heuristicScore(data: LegalMoveData): number {
+    let score = 1;
+
     if (data.isPromoting) {
-        return 1000;
+        score += 1000;
     } else if (data.isCapturing) {
         const attackerValue = Values[data.piece.id as keyof typeof Values];
         if (!data.capturedPiece) return 100;
 
-        const victimId = data.capturedPiece!.id;
+        const victimId = data.capturedPiece.id;
         const victimValue = Values[victimId as keyof typeof Values];
 
-        return 100 * victimValue - attackerValue;
+        score += 100 * victimValue - attackerValue;
     } else if (data.isChecking) {
-        return 300;
+        score += 300;
     }
 
-    return 1;
+    const file = FILES.indexOf(data.pos[0]);
+    const rank = +data.pos[1];
+
+    if (file === 4 || file === 5) {
+        score += 200;
+    } else if (file >= 2 && file <= 5) {
+        score += 100;
+    }
+
+    if (
+        data.piece.id === "p" &&
+        ((data.piece.color === "white" && rank > 2) ||
+            (data.piece.color === "black" && rank < 7))
+    ) {
+        score += 50;
+    }
+
+    return score;
 }
